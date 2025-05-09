46ee642b1adcd3ac2d4531095ee3efbffbf7ef45
Szász István Zoltán <istvanszasz99@gmail.com>
2025-05-09 19:44:16 +0200

    ESLint fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,25 +2,15 @@ module.exports = {
   env: {
     browser: true,
     commonjs: true,
-    es2021: true
+    es2021: true,
+    node: true
   },
   extends: 'standard',
-  overrides: [
-    {
-      env: {
-        node: true
-      },
-      files: [
-        '.eslintrc.{js,cjs}'
-      ],
-      parserOptions: {
-        sourceType: 'script'
-      }
-    }
-  ],
   parserOptions: {
-    ecmaVersion: 'latest'
+    ecmaVersion: 'latest',
+    sourceType: 'module'
   },
   rules: {
+    // Add your custom rules here
   }
 }
